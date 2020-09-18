2795736a0a4393cace348179e3543d9a800ec254
Michael Venema <michael.venema2010@gmail.com>
2020-09-18 12:42:33 -0700

    Update MerakiPortQuery.ps1

--- MerakiPortQuery.ps1
+++ MerakiPortQuery.ps1
@@ -1,21 +1,22 @@
 ###################################################
 # Meraki Switch Port Access Policy Grabber Script #
-#        Written By: Michael Venema V1.0          #
+#        Written By: Michael Venema V1.1          #
 ###################################################
+
 
 if (Test-Path "C:\scripts\export_test.csv") {
     Remove-Item -Path "C:\scripts\export_test.csv"
 }
 
-$apikey = Read-Host
-$orgID = Read-Host
+$apikey = Read-Host "Please Enter your Meraki API Key"
+$orgID = Read-Host "Please Enter your Meraki Org ID"
 
 $headers = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
 $headers.Add("Accept", "*/*")
 $headers.Add("X-Cisco-Meraki-API-Key", $apikey)
 
 $oresponse = Invoke-RestMethod "https://api.meraki.com/api/v0/organizations/$orgID/inventory" -Method 'GET' -Headers $headers -Body $body
-$switches = $oresponse  | Where-Object { $_.Name -like '*SW*' } | Select-Object serial, Name
+$switches = $oresponse  | Where-Object { $_.Model -like '*MS*' } | Select-Object serial, Name
 
 $export = "C:\scripts\export_test.csv"
 
@@ -26,11 +27,10 @@ $switches | ForEach-Object {
         $name = $_.Name
         $url = "https://api.meraki.com/api/v0/devices/$($_.serial)/switchPorts"
         $response = Invoke-RestMethod $url -Headers $headers -Body $body
-        $response | Select-Object @{L = 'SwitchName'; E = { $name } }, number, name, tags, type, vlan, accessPolicyNumber    | Export-Csv $export -Append -NoTypeInfo
+        $response | Select-Object @{L = 'SwitchName'; E = { $name } }, * | Convert-OutputForCsv | Export-Csv $export -Append -NoTypeInfo
         Write-Host "Added $name port settings to CSV File!" -ForegroundColor Green
     
     }
 
     catch { Write-host "Cannot connect to $name! Not adding to CSV" -ForegroundColor Red }
-
 }
